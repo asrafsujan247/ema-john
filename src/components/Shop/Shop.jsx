e5b01cb1b4592d0497812ac70235279a8a90a1cb
asrafsujan247 <asraf.sujan247@gmail.com>
2023-03-28 15:39:51 +0600

    added product image and name

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 import "./Shop.css";
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -7,10 +8,13 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
   return (
     <div className="shop-container">
       <div className="product-container">
-        <h3>products</h3>
+        {products.map((product) => (
+          <Product product={product} key={product.id}></Product>
+        ))}
       </div>
       <div className="cart-container">
         <h4>Order Summary</h4>
