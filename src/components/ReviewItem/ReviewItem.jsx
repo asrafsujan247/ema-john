1f20d13013464cd32f9ec9ae9e345918cc0361b3
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-07 15:20:49 +0600

    added orders section  showed cart products

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ReviewItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+
+const ReviewItem = ({ product }) => {
+  const { id, name, img, price, quantity } = product;
+  return (
+    <div className="review-item">
+      <img src={img} alt="" />
+      <div className="review-details">
+        <p className="product-title">{name}</p>
+        <p>
+          Price: <span className="orange-text">${price}</span>
+        </p>
+        <p>
+          Order Quantity: <span className="orange-text">{quantity}</span>
+        </p>
+        <p></p>
+      </div>
+      <button className="btn-delete">
+        <FontAwesomeIcon className="delete-icon" icon={faTrashAlt} />
+      </button>
+    </div>
+  );
+};
+
+export default ReviewItem;
---
+++ src/loaders/cartProductsLoader.js
@@ -0,0 +1,25 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductsLoader = async () => {
+  const loadedProducts = await fetch("products.json");
+  const products = await loadedProducts.json();
+
+  // if cartData is in data base you have to use async await
+
+  const storedData = getShoppingCart();
+  const savedCart = [];
+
+  console.log(storedData);
+  for (const id in storedData) {
+    const addedProduct = products.find((pd) => pd.id === id);
+    if (addedProduct) {
+      const quantity = storedData[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
+    }
+  }
+
+  return savedCart;
+};
+
+export default cartProductsLoader;
