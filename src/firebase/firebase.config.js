2ddc246b57ba3a5eabbaac8af1d64c4e6f251cbc
asrafsujan247 <asraf.sujan247@gmail.com>
2023-04-30 19:44:33 +0600

    added signup route and made some styles in login and also added font style

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7Damoc8leciYrU_xjj3LOGkPamXagITE",
+  authDomain: "ema-john-ca1bf.firebaseapp.com",
+  projectId: "ema-john-ca1bf",
+  storageBucket: "ema-john-ca1bf.appspot.com",
+  messagingSenderId: "525330028139",
+  appId: "1:525330028139:web:543a3d94fd743d02ad832e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
