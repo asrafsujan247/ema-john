ce2575c6100a837f01e77eb963ae10d533812e17
asrafsujan247 <asraf.sujan247@gmail.com>
2023-05-01 23:10:24 +0600

    added login redirect feature and private route also added hide and show password feature

--- src/__steps__.js
+++ src/__steps__.js
@@ -14,3 +14,26 @@
  * 4. set the context value
  * 5. set the provider
  * */
+
+/**
+ * HOSTING
+ * ---------------------------
+ * One time per pc
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ *
+ * For each project one time
+ * 1. firebase init
+ * 2. proceed
+ * 3. Hosting: firebase(up and down arrow key to move) and to select use space bar
+ * 4. existing project
+ * 5. select the project carefully
+ * 6. which project as public directory : dist (if you are using vite)
+ * 7. single page application : yes
+ * 8. continuous deployment : no
+ *
+ * For every time deploy
+ * 1. npm run build
+ * 2. firebase deploy
+ *
+ * */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 
 const Login = () => {
+  const [show, setShow] = useState(false);
+
   const { signIn } = useContext(AuthContext);
+
+  // navigation in private route
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
+  const from = location?.state?.from?.pathname || "/";
 
   const handleLogin = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email, password);
 
     signIn(email, password)
       .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
         form.reset();
+        navigate(from, { replace: true }); //navigate in the previous route
       })
       .catch((error) => {
         console.log(error.message);
@@ -34,7 +42,15 @@ const Login = () => {
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="" required />
+          <input
+            type={show ? "text" : "password"}
+            name="password"
+            id=""
+            required
+          />
+          <p onClick={() => setShow(!show)}>
+            {show ? <span>Hide password</span> : <span>Show password</span>}
+          </p>
         </div>
         <input className="btn-submit" type="submit" value="Login" />
       </form>
